71245240ee5efb014d0bf0682566a207f4e625cd
Salta1414 <saltabadly@gmail.com>
2025-07-21 11:35:00 +0200

    init 2

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    ignores: ['node_modules/**'],
+    rules: {
+    }
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module'
+    },
+    rules: {
+    }
+  }
+];
