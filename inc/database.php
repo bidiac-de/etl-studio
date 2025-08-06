1098a46f03e9d150984903e59c429fc257640442
Sven König <info.micgram.de>
2025-08-06 20:06:37 +0200

    bug fix database

--- inc/database.php
+++ inc/database.php
@@ -13,7 +13,7 @@
         openDatabase();
         $db->exec("CREATE TABLE IF NOT EXISTS jobs (jobID INTEGER PRIMARY KEY AUTOINCREMENT, jobName TEXT NOT NULL, jobCreatedBy INTEGER NOT NULL DEFAULT '0', jobCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP)");
         $db->exec("CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, fullname TEXT NOT NULL DEFAULT '')");
-        $db->exec("CREATE TABLE IF NOT EXISTS servers (serverID INTEGER PRIMARY KEY AUTOINCREMENT, host TEXT NOT NULL, access_key TEXT NOT NULL, description TEXT NOT NULL DEFAULT ''");
+        $db->exec("CREATE TABLE IF NOT EXISTS servers (serverID INTEGER PRIMARY KEY AUTOINCREMENT, host TEXT NOT NULL, access_key TEXT NOT NULL, description TEXT NOT NULL DEFAULT '')");
 
         $username = $db->escapeString($username);
         $password = hash("sha256", "***$username###$password***somesalt<3");
